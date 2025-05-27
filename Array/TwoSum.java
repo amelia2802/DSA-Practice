1c3db24ecabc6f3abf94742c9067d4ee6291a61b
Amelia Dutta <amelia280111dutta@gmail.com>
2025-05-27 12:43:52 +0530

    Implement TwoSum algorithm

---
+++ Array/TwoSum.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+@author Amazon, Google, Microsoft, Facebook (Meta), Apple, Bloomberg, Uber, LinkedIn
+
+T.C:O(n)
+S.C:O(n)
+ */
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> tempMap = new HashMap<>();
+
+        for(int i =0;i< nums.length;i++ ){
+            int num2 = target - nums[i];
+            if(tempMap.containsKey(num2)){
+                return new int[]{tempMap.get(num2),i};
+            }
+            tempMap.put(nums[i],i);
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum found.");
+        }
+    }
+}
