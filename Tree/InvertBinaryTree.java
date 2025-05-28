585aa4d1068ff7ad8256e7d38aecb56c78a2c34a
Amelia Dutta <amelia280111dutta@gmail.com>
2025-05-28 23:16:51 +0530

    Implement InvertBinaryTree

---
+++ Tree/InvertBinaryTree.java
@@ -0,0 +1,73 @@
+/*
+ * Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+@author Google
+
+T.C:O(n)
+S.C:O(h): The space complexity is determined by the recursion stack, which depends on the height h of the tree. In the worst case (skewed tree), h = n. In the best case (balanced tree), h = log(n).
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class InvertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        invertTree(root);
+
+
+    }
+}
